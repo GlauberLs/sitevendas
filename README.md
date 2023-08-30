1f193ec99a4f57f47a12ec8e945adf6ad97f7d83
Glauber Luis Alves dos Santos <ART0052835@aluno.firjansenaisesi.com.br>
2023-08-30 08:51:17 -0300

    SiteVendas

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Site de vendas para testes
 
 <h2> Objetivo do site </h2>
 O objetivo do site tem como objetivo mostrar como montar um site de venda
+
+ ht
---
+++ js/nav.js
@@ -0,0 +1,21 @@
+
+class navComponente{
+    CriarNovoMenu(IdDoMenu,Elementos){
+        let MenuEscolhido = document.getElementById(IdDoMenu)
+        MenuEscolhido.innerHTML+="<ul id='Menu_02'> </ul>"
+        let MenuAcrescentar = document.getElementById("Menu_02")
+        Elementos.forEach(MenuAdd => {
+            MenuAcrescentar.innerHTML+="<li><a href='#'>"+MenuAdd+"</a></li>"
+            
+        });
+
+    }
+    CriarBarraNavUsuario(IdDoMenu,Localizacao){
+        let MenuEscolhido = document.getElementById(IdDoMenu)
+        MenuEscolhido.innerHTML+="<div class='localnav'><h4>"+Localizacao+"</h4></div>"
+    
+    }
+
+}
+
+export default navComponente
